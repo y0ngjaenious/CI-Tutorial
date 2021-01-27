16c8d46d10e693d9d4f375f5cbfe5f35cd9b10c2
y0ngjaenious <>
2021-01-27 15:48:56 +0900

    [ADD] Setting for CodeDeploy

---
+++ Scripts/pullDocker.sh
@@ -0,0 +1,14 @@
+# docker login
+docker login -u y0ngjaenious -p dy1013778!
+
+# pull docker image
+if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
+then
+  docker-compose -f /deploy/docker-compose.yml pull
+elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
+then
+  docker-compose -f /deploy/docker-compose.stage.yml pull
+elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
+then
+  docker-compose -f /deploy/docker-compose.yml pull
+fi
---
+++ Scripts/runDocker.sh
@@ -0,0 +1,19 @@
+if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
+then
+  # Remove any anonymous volumes attached to containers
+  docker-compose -f /deploy/docker-compose.yml rm -v 
+  # build images and run containers
+  docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
+elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
+then
+  # Remove any anonymous volumes attached to containers
+  docker-compose -f /deploy/docker-compose.stage.yml rm -v 
+  # build images and run containers
+  docker-compose -f /deploy/docker-compose.stage.yml up --detach --renew-anon-volumes
+elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
+then
+  # Remove any anonymous volumes attached to containers
+  docker-compose -f /deploy/docker-compose.yml rm -v 
+  # build images and run containers
+  docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
+fi
