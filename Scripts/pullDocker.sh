346705eff258988c89844ca7687358ab267cb34e
y0ngjaenious <>
2021-01-27 16:40:40 +0900

    [TEST] CICD

--- Scripts/pullDocker.sh
+++ Scripts/pullDocker.sh
@@ -4,7 +4,7 @@ docker login -u y0ngjaenious -p dy1013778!
 # pull docker image
 if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
-  docker-compose -f /deploy/docker-compose.yml pull
+  docker-compose -f /home/ec2-user/docker-compose.yml pull
 elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
 then
   docker-compose -f /deploy/docker-compose.stage.yml pull
