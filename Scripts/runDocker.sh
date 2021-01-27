0c3409e0dfaaf6d4a01b4b23597eead686f10586
y0ngjaenious <>
2021-01-27 19:12:51 +0900

    m

--- Scripts/runDocker.sh
+++ Scripts/runDocker.sh
@@ -1,7 +1,12 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
   # Remove any anonymous volumes attached to containers
-  docker-compose -f /home/ec2-user/docker-compose.yml rm -v 
+  docker-compose -f /home/ec2-user/docker-compose.yml rm -v
+
+  mkdir frontend
+  mkdir backend
+  mkdir nginx
+  mkdir mysql
   # build images and run containers
   docker-compose -f /home/ec2-user/docker-compose.yml up --detach --renew-anon-volumes
 elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
